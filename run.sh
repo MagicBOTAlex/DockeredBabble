830f7589a47e003967f63208f069f2e342eec82e
BOTAlex <zhentao2004@gmail.com>
2025-04-24 23:24:42 +0000

    My final version before calibration

--- run.sh
+++ run.sh
@@ -7,6 +7,9 @@ IMAGE_NAME="dockered-babble"
 echo "Building Docker image..."
 docker build -t $IMAGE_NAME .
 
+docker stop babble
+docker remove babble
+
 # Run the Docker container
 echo "Running the container..."
-docker run --network host --gpus=all --shm-size=1g -it $IMAGE_NAME
+docker run --network host --gpus=all --shm-size=1g --name babble -it $IMAGE_NAME
